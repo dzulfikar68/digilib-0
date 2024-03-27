bd5a2b77506f3562f0de0a1dd1a93f88ea07c7aa
Dzulfikar Fauzi <dzulfikar68.fauzi@gmail.com>
2024-03-27 09:58:39 +0700

    fixing login mhs php mysqli php 7

--- login_mhs.php
+++ login_mhs.php
@@ -7,10 +7,20 @@
 	$password=$_POST["password"];
 	
 	if(count($_POST) > 0) {
-		$conn = mysql_connect("ap-cdbr-azure-southeast-b.cloudapp.net","bf00621f72c1dc","76435f6b");
-		mysql_select_db("db_perpus",$conn);
-		$result = mysql_query("SELECT * FROM mahasiswa WHERE nim='" . $nim . "' and password = '". $password."'");
-		$row  = mysql_fetch_array($result);
+		$conn = mysqli_connect("localhost", "root", "");
+
+		$db_select = mysqli_select_db($conn, "db_perpus");
+		if (!$db_select){
+			die ("Could not select the database: <br />".mysqli_error($conn));
+		}
+		
+		$q = "SELECT * FROM mahasiswa WHERE nim='" . $nim . "' and password = '". $password."'";
+		$result = mysqli_query($conn, $q);
+		if (!$result){
+			die ("Could not query the database: <br />". mysqli_error($conn));
+		}	
+
+		$row = mysqli_fetch_array($result);
 
 		if(is_array($row)) {
 			$_SESSION["nama"] = $row[nama];
